2ee7303682bb118751ccda4847de461845d9761a
Assassin318554 <nahinsikdar123@gmail.com>
2024-07-10 02:51:45 +0600

    a

--- next-auth/app/auth/[...nextauth]/route.js
+++
@@ -1,5 +0,0 @@
-import NextAuth from "next-auth/next";
-import { authOptions } from "@/app/nextAuth/nextAuthOptions";
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
